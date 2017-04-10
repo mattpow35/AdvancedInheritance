1880982b87a1b64fc012aef08222f0d52355ea05
mattpow35 <mpow2231@740s-pl-121.local>
2017-04-10 09:42:32 -0600

    added the pizzable interface

---
+++ src/inheritance/model/pizzable.java
@@ -0,0 +1,18 @@
+package inheritance.model;
+
+/**
+ * 
+ * @author Matt Powley
+ * @author Kadin Hatchell
+ * @author Ammar Nazir
+ * @author Jack Machara
+ * @author Aaron Farr
+ */
+
+public interface pizzable 
+{
+	public boolean hasPineapple();
+	public void toss();
+	public void setPepporoni(int pepporoniCount);
+	public int getPepporoni();
+}
